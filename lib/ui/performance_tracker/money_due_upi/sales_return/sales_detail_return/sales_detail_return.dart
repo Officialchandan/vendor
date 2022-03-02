ef171f826b53b9e624a1baa894d5f165773b1076
vaibhav gour <vaibhav.gour@tekzee.com>
2022-03-02 14:54:40 +0530

    "2 march vaibhav 4"

---
+++ lib/ui/performance_tracker/money_due_upi/sales_return/sales_detail_return/sales_detail_return.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SalesReturnDetail extends StatefulWidget {
+  const SalesReturnDetail({Key? key}) : super(key: key);
+
+  @override
+  _SalesReturnDetailState createState() => _SalesReturnDetailState();
+}
+
+class _SalesReturnDetailState extends State<SalesReturnDetail> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(), body: Container());
+  }
+}
